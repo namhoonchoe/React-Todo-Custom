40ac5cc845a904d2937d0b3ec201e0332acf9990
Namhoon Choe <33464899+namhoonchoe@users.noreply.github.com>
2024-07-24 21:29:53 +0900

    [Feat]리스트 U/D
    리스트 이름 수정 및 삭제 기능 추가

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,13 +1,20 @@
 import { styled } from "styled-components";
-import { useRecoilValue } from "recoil";
-import { taskState } from "../atoms";
+import { useRecoilValue, useRecoilState } from "recoil";
+import { taskState,categoryState } from "../atoms";
 import AddCard from "./AddCard";
 import Card from "./Card";
 import DropDown from "./UI/DropDown";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import DismissIcon from "./Icons/DismissIcon";
-import { ButtonContainer } from "./UI/StyledFragments";
- 
+import {
+  ButtonContainer,
+  TextButton,
+  Form,
+  FormInput,
+  FormButton,
+} from "./UI/StyledFragments";
+import Modal from "./UI/Modal";
+import { useForm } from "react-hook-form";
 
 const ListLayout = styled.section`
   display: flex;
@@ -61,12 +68,12 @@ const DropDownHeader = styled.header`
   align-items: center;
   width: 100%;
 `;
- 
+
 const AnimatedButton = styled(ButtonContainer)`
-&:hover {
-  background-color: #3b506d;
-}
-`
+  &:hover {
+    background-color: #3b506d;
+  }
+`;
 
 const MenuContainer = styled.div`
   display: flex;
@@ -75,6 +82,43 @@ const MenuContainer = styled.div`
   align-items: center;
   padding: 0.5rem;
   gap: 0.5rem;
+`;
+ 
+
+const TaskTitle = styled.p`
+  color: #fff;
+  text-transform: capitalize;
+  font-weight: 600;
+`;
+
+const EditButton = styled(TextButton)`
+  background-color: #579dff;
+  &:hover {
+    background-color: #1e22f5;
+  }
+`;
+
+const DeleteButton = styled(TextButton)`
+  background-color: #f74242;
+  &:hover {
+    background-color: #f51e1e;
+  }
+`;
+
+const TaskContainer = styled.section`
+  display: flex;
+  flex-shrink: 0;
+  flex-grow: 0;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #1f2937;
+  border-radius: 12px;
+  width: 100%;
+  height: 5rem;
+  gap: 0.5rem;
+  color: #fff;
+  text-transform: capitalize;
+  padding: 1rem;
 `;
 
 type ListProps = {
@@ -90,41 +134,134 @@ export default function List({ categoryName, categoryId }: ListProps) {
     .reverse();
 
   const toggleOpen = () => setIsOpen((prev) => !prev);
+  const editDialogRef = useRef<HTMLDialogElement | null>(null);
+  const deleteDialogRef = useRef<HTMLDialogElement | null>(null);
+  const clearDialogRef = useRef<HTMLDialogElement | null>(null);
+
+
+  const { register, handleSubmit } = useForm<SubmitType>();
+  const [categories , setCategories] = useRecoilState(categoryState);
+
+  type SubmitType = {
+    categoryName: string;
+  };
+
+  const deleteTodo = (id: string) => {
+    const newList = categories.filter((category) => category.categoryId !== id);
+    setCategories(newList);
+    deleteDialogRef.current?.close();
+  };
+
+
+
+  const submitHandler = ({ categoryName }: SubmitType) => {
+    let targetCategory = categories.find((category) => category.categoryId === categoryId);
+    const targetIndex = categories.findIndex((category) => category.categoryId === categoryId);
+    if (targetCategory) {
+      targetCategory = { ...targetCategory, categoryName: categoryName };
+      return setCategories([
+        ...categories.slice(0, targetIndex),
+        targetCategory,
+        ...categories.slice(targetIndex + 1),
+      ]);
+    }
+    editDialogRef.current?.close();
+  };
+
 
   return (
-    <ListLayout>
-      <ListHeader>
-        <ListTitle>{categoryName}</ListTitle>
-        <DropDown isOpen={isOpen} toggleOpen={toggleOpen}>
-          <DropDownHeader>
-            <ListTitle>List actions</ListTitle>
-            <AnimatedButton onClick={toggleOpen}>
-              <DismissIcon />
-            </AnimatedButton>
-          </DropDownHeader>
-          <MenuContainer>
-            <ListTitle>Change list title</ListTitle>
-          </MenuContainer>
-          <MenuContainer>
-            <ListTitle> Delete List</ListTitle>
-          </MenuContainer>
-          <MenuContainer>
-            <ListTitle>clear list items</ListTitle>
-          </MenuContainer>
-        </DropDown>
-      </ListHeader>
-      <ListMain>
-        {filteredTasks.map((task) => (
-          <Card
-            key={task.taskId}
-            task={task.task}
-            taskId={task.taskId}
-            categoryName={categoryName}
-          />
-        ))}
-      </ListMain>
+    <>
+      <ListLayout>
+        <ListHeader>
+          <ListTitle>{categoryName}</ListTitle>
+          <DropDown isOpen={isOpen} toggleOpen={toggleOpen}>
+            <DropDownHeader>
+              <ListTitle>List actions</ListTitle>
+              <AnimatedButton onClick={toggleOpen}>
+                <DismissIcon />
+              </AnimatedButton>
+            </DropDownHeader>
+            <MenuContainer onClick={() => editDialogRef.current?.showModal()}>
+              <ListTitle>Change list title</ListTitle>
+            </MenuContainer>
+            <MenuContainer onClick={() => deleteDialogRef.current?.showModal()}>
+              <ListTitle> Delete List</ListTitle>
+            </MenuContainer>
+            <MenuContainer>
+              <ListTitle>clear list items</ListTitle>
+            </MenuContainer>
+          </DropDown>
+        </ListHeader>
+        <ListMain>
+          {filteredTasks.map((task) => (
+            <Card
+              key={task.taskId}
+              task={task.task}
+              taskId={task.taskId}
+              categoryName={categoryName}
+            />
+          ))}
+        </ListMain>
 
-      <AddCard categoryName={categoryName} categoryId={categoryId} />
-    </ListLayout>
+        <AddCard categoryName={categoryName} categoryId={categoryId} />
+      </ListLayout>
+      {/**
+       * Edit Dialog
+       */}
+      <Modal modalMessage={categoryName} ref={editDialogRef}>
+        <Form onSubmit={handleSubmit(submitHandler)}>
+          <FormInput
+            placeholder={categoryName}
+            {...register("categoryName", {
+              required: "Please write a board name",
+            })}
+          />
+          <FormButton>
+            <EditButton
+              type="submit"
+              onClick={() => editDialogRef.current?.close()}
+            >
+              Edit task
+            </EditButton>
+
+            <form method="dialog">
+              <ButtonContainer>
+                <DismissIcon />
+              </ButtonContainer>
+            </form>
+          </FormButton>
+        </Form>
+      </Modal>
+      {/**
+       * Delete Dialog
+       */}
+      <Modal
+        modalMessage={"정말로 삭제 하시겠습니까?"}
+        ref={deleteDialogRef}
+        mainStyle={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "start",
+          padding: "1rem  0.5rem",
+          gap: "0.5rem",
+        }}
+      >
+        <TaskContainer>
+          <TaskTitle>{categoryName}</TaskTitle>
+        </TaskContainer>
+
+        <FormButton>
+          <DeleteButton type="submit" onClick={() => deleteTodo(categoryId)}>
+            Delete task
+          </DeleteButton>
+
+          <form method="dialog">
+            <ButtonContainer>
+              <DismissIcon />
+            </ButtonContainer>
+          </form>
+        </FormButton>
+      </Modal>
+    </>
   );
 }
